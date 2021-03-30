605c3ddf9fd4166e27456c7a7069912c2d6c82f0
Kevin Li <chunxli@microsoft.com>
2021-03-30 10:06:04 +0800

    comment out post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,7 +1,7 @@
-if [[ $PLATFORM == "Android" ]]; then
-    echo "Generate CodePush release to Android app."
-    appcenter codepush release-react -a KevinApps/RSSReader-RN-Android -d Staging -k ./private.pem --token $API_Token
-else
-    echo "Generate CodePush release to iOS app."
-    appcenter codepush release-react -a KevinApps/RSSReader-RN-iOS -d Staging -k ./private.pem --token $API_Token
-fi
+# if [[ $PLATFORM == "Android" ]]; then
+#     echo "Generate CodePush release to Android app."
+#     appcenter codepush release-react -a KevinApps/RSSReader-RN-Android -d Staging -k ./private.pem --token $API_Token
+# else
+#     echo "Generate CodePush release to iOS app."
+#     appcenter codepush release-react -a KevinApps/RSSReader-RN-iOS -d Staging -k ./private.pem --token $API_Token
+# fi
